96bf65a1d178617a391d521d3f46c411298e3325
4ybakut2004 <4ybakut2004@gmil.com>
2014-02-27 16:56:15 +0700

    before first deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.1.0'
 
 #set :application, 'my_app_name'
 #set :repo_url, 'git@example.com:me/my_repo.git'
-set :repo_url, 'git@github.com:4ybakut2004/NewVend.git'
+set :repo_url, 'git@github.com:4ybakut2004/NewVendTest.git'
 set :application, 'NewVend'
 application = 'NewVend'
 set :rvm_type, :user
