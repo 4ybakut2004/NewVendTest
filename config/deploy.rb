5e013693c77d1a8f3ef306715ab671e2531c6852
4ybakut2004 <4ybakut2004@gmil.com>
2014-03-11 11:31:51 +0700

    dadasd

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ lock '3.1.0'
 #set :repo_url, 'git@example.com:me/my_repo.git'
 #set :repo_url, 'git@github.com:4ybakut2004/NewVendTest.git'
 set :repo_url, 'file:///home/chybakut2004/Workspace/RubyOnRails/NewVend/.git'
+set :deploy_via, :copy
 set :application, 'NewVend'
 application = 'NewVend'
 set :rvm_type, :user
