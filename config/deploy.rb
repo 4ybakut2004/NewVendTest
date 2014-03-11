105b29bd4c5e7a6a5a46a57dcd7d9435121c59a9
4ybakut2004 <4ybakut2004@gmil.com>
2014-03-11 12:28:42 +0700

    dsadasd

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :application, 'NewVend'
 set :deploy_to, '/var/www/apps/NewVend'
 set :deploy_via, :copy
 set :scm, :git
-set :repo_url, 'file://.git'
+set :repo_url, 'file:///vagrant/NewVend.git'
 application = 'NewVend'
 set :rvm_type, :user
 set :rvm_ruby_version, '2.0.0-p247'
