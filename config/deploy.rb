371b628f5a104978008c135910987fd0f9508457
4ybakut2004 <4ybakut2004@gmil.com>
2014-04-07 10:20:36 +0700

    deploy to new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,24 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-#set :repo_url, 'git@github.com:4ybakut2004/NewVendTest.git'
-#set :application, 'NewVend'
-#application = 'NewVend'
-#set :rvm_type, :user
-#set :rvm_ruby_version, '2.0.0-p247'
-#set :deploy_to, '/var/www/apps/NewVend'
-#set :port, 2222
-
+set :repo_url, 'git@github.com:4ybakut2004/NewVendTest.git'
 set :application, 'NewVend'
-set :deploy_to, '/var/www/apps/NewVend'
-set :deploy_via, :copy
-#set :scm, :git
-set :repo_url, 'file:///vagrant/NewVend/.git'
-set :branch, 'production'
 application = 'NewVend'
 set :rvm_type, :user
 set :rvm_ruby_version, '2.0.0-p247'
+set :deploy_to, '/var/www/apps/NewVend'
 set :port, 2222
+
+#set :application, 'NewVend'
+#set :deploy_to, '/var/www/apps/NewVend'
+#set :deploy_via, :copy
+#set :scm, :git
+#set :repo_url, 'file:///vagrant/NewVend/.git'
+#set :branch, 'production'
+#application = 'NewVend'
+#set :rvm_type, :user
+#set :rvm_ruby_version, '2.0.0-p247'
+#set :port, 2222
 
 namespace :foreman do
   desc 'Start server'
