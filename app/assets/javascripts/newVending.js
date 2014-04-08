c4f606930c5f9e343dce1fc31aa0b7f08b7aa6aa
4ybakut2004 <4ybakut2004@gmil.com>
2014-04-08 11:38:29 +0700

    employees on angular

--- app/assets/javascripts/newVending.js
+++ app/assets/javascripts/newVending.js
@@ -1,27 +1,5 @@
 var newVending = angular.module('newVending', ['ngResource', 'ngRoute']);
 
-/*newVending.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/employees', {
-        controller: 'EmployeesCtrl',
-        templateUrl: 'employees/'
-      }).
-      otherwise({
-      	controller: 'ApplicationCtrl',
-        redirectTo: '/'
-      });
-  }]);*/
-
-$(document).on('page:load', function() {
+$(document).on('ready page:load', function() {
   angular.bootstrap(document.getElementById('newVending'), ['newVending']);
-});
-
-newVending.run(function($compile, $rootScope, $document) {
-  return $document.on('page:load', function() {
-    var body, compiled;
-    body = angular.element('body');
-    compiled = $compile(body.html())($rootScope);
-    return body.html(compiled);
-  });
 });
