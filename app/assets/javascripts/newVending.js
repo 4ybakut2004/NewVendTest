3d831e3a2f076509ec2a767a56badf3e42b5492d
4ybakut2004 <4ybakut2004@gmil.com>
2014-04-21 17:25:09 +0700

    compile

--- app/assets/javascripts/newVending.js
+++ app/assets/javascripts/newVending.js
@@ -4,54 +4,7 @@ $(document).on('ready page:load', function() {
   angular.bootstrap(document.getElementById('newVending'), ['newVending']);
 });
 
-function DoubleScroll($compile, $window) 
-{       
-    return {
-        link: function($scope, element, attrs) {
-        	$scope.scrollBarWidth = function() {
-        		return {
-					width: element.width()
-				};
-        	};
-
-        	$scope.scrollContentWidth = function() {
-        		return {
-					width: element.find('table').first().width()
-				};
-        	};
-
-			var scrollBar = $compile('<div ng-style="scrollBarWidth()"></div>')($scope);
-			var scrollContent = $compile('<div ng-style="scrollContentWidth()"></div>')($scope);
-
-			scrollBar.addClass('scroll');
-
-			function setSize() {
-				scrollBar.css({
-					width: element.width()
-				});
-
-				scrollContent.css({
-					width: element.find('table').first().width()
-				});
-			}
-			
-			angular.element($window).bind('resize', setSize);
-			element.scroll(function() {
-		        scrollBar.scrollLeft(element.scrollLeft());
-		    });
-		    scrollBar.scroll(function() {
-		        element.scrollLeft(scrollBar.scrollLeft());
-		    });
-
-			scrollBar.append(scrollContent);
-			scrollBar.insertBefore(element);
-        }
-    };
-}
-
-newVending.directive('doublescroll', DoubleScroll);
-
-function FixedHeader($compile, $window) 
+function FixedHeader($compile) 
 {       
     return {
 	    link: function($scope, element, attrs) {
@@ -109,4 +62,4 @@ function FixedHeader($compile, $window)
     };
 }
 
-newVending.directive('fixedheader', FixedHeader);
+newVending.directive('fixedheader', ['$compile', FixedHeader]);
